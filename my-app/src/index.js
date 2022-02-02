6b49c7035a49cb1047d478c44aeeb8297008851d
Natasha Ramburrun <natasha.ramburrun@mmtdigital.co.uk>
2022-02-02 11:30:05 +0000

    added the route into the project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import './styles/styles.scss';
 import Home from './component/Home';
+import Location from './component/Location';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById("root");
@@ -13,6 +14,7 @@ render(
   <BrowserRouter>
    <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="location" element={<Location />} />
     </Routes>
   </BrowserRouter>,
   rootElement
